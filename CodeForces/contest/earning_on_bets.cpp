7351dc6f1675e3f9959fd2bf4c9611964363ef58
Safaet-ullah <shafetullah02@gmail.com>
2024-06-07 23:01:00 +0600

    contest 1979

---
+++ CodeForces/contest/earning_on_bets.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000000
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int gcd(int a,int b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    unsigned ll int t,n,x,y,a,b;
+    unsigned ll lcm;
+    vl data;
+    cin >> t;
+    unsigned ll int g,lc;
+    while (t--)
+    {
+        cin>>n;
+        fo(x,n)
+        {
+            cin>>a;
+            data.pb(a);
+        }
+        lcm=data[0];
+        for(int i=1;i<n;i++)
+        {
+            g=(gcd(lcm,data[i]));
+            lc=lcm*data[i];
+            lcm=lc/g;
+            
+        }
+        //cout<<" lcm "<<lcm<<endl;
+        unsigned ll int sum=0;
+        for(int i=0;i<n;i++)
+        {
+           sum += (lcm/data[i]);
+        }
+        //cout<<" sum "<<sum<<endl;
+        if(sum>=lcm)
+        {
+            cout<<-1<<endl;
+            data.clear();
+            continue;
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<lcm/data[i]<<" ";
+        }
+        cout<<endl;
+        data.clear();
+
+    }
+    return 0;
+}
+/** মৃতরা কি ব্যাথা পায় ? যখন দেহ মাটিতে খায় ! **/
+/**LaZyBadB0y **/
---
+++ CodeForces/contest/xor_sequence.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t,n,x,y,a,b;
+    cin >> t;
+    while (t--)
+    {
+        cin>>x>>y;
+        a=x^y;
+        b=1;
+        while(true)
+        {
+            //cout<<b<<" b "<<endl;
+            if(a&b)
+            {
+                cout<<b<<endl;
+                break;
+
+            }
+            b=b*2;
+        }
+    }
+    return 0;
+}
+/** মৃতরা কি ব্যাথা পায় ? যখন দেহ মাটিতে খায় ! **/
+/**LaZyBadB0y **/
